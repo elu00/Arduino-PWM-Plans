626abc380af6e7776388c1fa0f2a4876897c5057
Ethan Lu <elu@davidsonacademy.unr.edu>
2017-08-27 19:30:08 -0700

    Add First Board Prototype

---
+++ Desktop_Client/PWM_C/PWM_C/InterfaceModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace PWM_C
+{
+    class InterfaceModel
+    {
+        
+        public InterfaceModel()
+        {
+
+        }
+    }
+}
